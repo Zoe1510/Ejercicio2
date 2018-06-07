cf5d2fa6ffd3a23ace39ee5a85d00a957b7a607d
Zoe1510 <patriciabermudez465@gmail.com>
2018-06-07 10:13:30 -0400

    añadiendo archivos

---
+++ Ejercicio2_DFDaCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_DFDaCSharp
+{
+    class Program
+    {
+        /* EJERCICIO2
+
+         CAPTURAR:
+         *  EL NOMBRE DEL VENDEDOR, 
+         *  el número de identificación y 
+         *  el valor mensual de ventas. 
+
+         LIQUIDAR LA COMISIÓN DE VENTA DE ACUERDO A LO SIGUIENTE:
+         * Si la venta es menor a 1500000 no hay comisión.
+         * Si la venta está entre 1500000 y 3500000 la comisión es del 5%. 
+         * Si es mayor 3500000 y 7000000 la comisión es del 10%, 
+         * si la venta está entre 7000000 y 15000000 la comisión será del 20% y 
+         * si es mayor del 15000000 la comisión será del 30%. 
+
+         SE DEBE IMPRIMIR UN REPORTE FINAL CON: 
+         * Nombre del vendedor, 
+         * número de identificación, 
+         * ventas realizadas, 
+         * comisión, (El descuento eps y pensión se aplica por el salario mínimo legal vigente más la comisión. )
+         * valor descuento de eps, 
+         * pensión, 
+         * subsidio de transporte (El subsidio de transporte aplicara su valor si Y solo si el salario mínimo legal vigente MÁS comisión es menor a 2 salarios mínimos legales vigentes.)
+         * total a pagar.
+         * 
+         * 
+
+        */
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
